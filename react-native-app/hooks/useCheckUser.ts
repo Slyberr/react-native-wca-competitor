f3daa89f536b018e1d17f94826a73ba29ab03dcc
Slyberr <slyber.demi.sel@gmail.com>
2026-04-29 11:41:10 +0200

    refactor app v2

--- react-native-app/hooks/useCheckUser.ts
+++ react-native-app/hooks/useCheckUser.ts
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
-const endpoint =
-  "https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/refs/heads/v1/persons/";
+const endpoint = process.env.URL + "persons/";
 
 export default function useCheckUser(ID: string) {
   return useQuery({
     queryKey: [ID],
     queryFn: async () => {
-      return await fetch(endpoint + ID + ".json").then((r) => {
+      return await fetch(endpoint + ID).then((r) => {
         if (!r.ok) {
           return false;
         } else {
---
+++ server/server.ts
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import express from "express";
+import { createConnection } from "mysql2/promise";
+
+const app = express();
+dotenv.config();
+
+// configure MariaDB
+const dbConfig = {
+  host: process.env.URL_MARIADB,
+  user: "root",
+  password: process.env.MDP_USER,
+  database: "WCA_DATA_04_27",
+};
+
+//Check if the user exist.
+app.get("/persons/:input", async (req: any, res: any) => {
+  try {
+    const input: string = req?.params?.input;
+    //Manage white spaces
+    input.replace(/\s+/g, " ");
+    const connection = await createConnection(dbConfig);
+    const [rows] = await connection.query(
+      "SELECT * FROM persons where  wca_id = ? OR name like ? ",
+      [input, `%${input}%`],
+    );
+    await connection.end();
+    return res.json(rows);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur started on :${PORT}`);
+});
